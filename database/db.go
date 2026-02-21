dc7890250bb8fbd271e9a9a4f36004a752f836fd
Paulo Sérgio <43386661+paulopsms@users.noreply.github.com>
2026-02-20 23:16:13 -0300

    first commit with github actions pipeline (#1)
    * first commit with github actions pipeline
    
    * fix workflow Lint
    
    * export database port
    
    * fix test on pipeline
    
    * fix test - using secrets for database connection

--- database/db.go
+++ database/db.go
@@ -2,7 +2,7 @@ package database
 
 import (
 	"log"
-
+	"os"
 	"github.com/guilhermeonrails/api-go-gin/models"
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
@@ -14,11 +14,17 @@ var (
 )
 
 func ConectaComBancoDeDados() {
-	stringDeConexao := "host=postgres user=root password=root dbname=root port=5432 sslmode=disable"
+    host := os.Getenv("DB_HOST")
+    user := os.Getenv("DB_USER")
+    password := os.Getenv("DB_PASSWORD")
+    name := os.Getenv("DB_NAME")
+    port := os.Getenv("DB_PORT")
+
+	stringDeConexao := "host="+ host + " user=" + user + " password=" + password + " dbname=" + name + " port=" + port + " sslmode=disable"
 	DB, err = gorm.Open(postgres.Open(stringDeConexao))
 	if err != nil {
 		log.Panic("Erro ao conectar com banco de dados")
 	}
 
-	DB.AutoMigrate(&models.Aluno{})
+	_ = DB.AutoMigrate(&models.Aluno{})
 }
